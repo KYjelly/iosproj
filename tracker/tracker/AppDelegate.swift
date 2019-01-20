e388be09a72b5b6bb3ecdbd530289082b4742b33
Louis Leong <louis@live.com.sg>
2019-01-20 11:36:50 +0800

    ..

--- tracker/tracker/AppDelegate.swift
+++ tracker/tracker/AppDelegate.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 import CoreData
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        MSAppCenter.start("4274b834-5633-46b9-ad29-532a0c5f3b14", withServices:[ MSAnalytics.self, MSCrashes.self ])
         return true
     }
 
